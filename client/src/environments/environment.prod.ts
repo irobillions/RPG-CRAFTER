f3a7f80fe557e53b4ec70c94c4ab774ec9eac856
irobilllions <christbouka14@yahoo.fr>
2024-12-27 17:56:21 -0500

    all project

---
+++ client/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    baseUrl: 'http://ec2-15-223-77-15.ca-central-1.compute.amazonaws.com:3000/',
+    serverUrl: 'http://ec2-15-223-77-15.ca-central-1.compute.amazonaws.com:3000/api',
+};
